bae6eff4ecc91449dedfeef645b15038ec17c839
finn <serafinn1993@gmail.com>
2019-10-20 09:38:46 -0400

    hometask_4

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -5,26 +5,27 @@ import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.lifecycle.Observer
-import kotlinx.android.synthetic.main.activity_profile.*
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.models.Profile
 import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
-
-class ProfileActivity : AppCompatActivity(){
+class ProfileActivity : AppCompatActivity() {
 
     companion object {
         const val IS_EDIT_MODE = "IS_EDIT_MODE"
     }
 
-    private lateinit var viewModel: ProfileViewModel
     var isEditMode = false
     lateinit var viewFields : Map<String, TextView>
+
+    private lateinit var viewModel: ProfileViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -32,6 +33,12 @@ class ProfileActivity : AppCompatActivity(){
         setContentView(R.layout.activity_profile)
         initViews(savedInstanceState)
         initViewModel()
+        Log.d("M_ProfileActivity", "onCreate")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(IS_EDIT_MODE, isEditMode)
     }
 
     private fun initViewModel() {
@@ -40,17 +47,17 @@ class ProfileActivity : AppCompatActivity(){
         viewModel.getTheme().observe(this, Observer { updateTheme(it) })
     }
 
-    private fun updateTheme(mode: Int) {
-        delegate.localNightMode = mode
-
-    }
-
     private fun updateUI(profile: Profile) {
         profile.toMap().also {
             for ((k, v) in viewFields) {
                 v.text = it[k].toString()
             }
         }
+    }
+
+    private fun updateTheme(mode: Int) {
+        Log.d("M_ProfileActivity", "updateTheme")
+        delegate.setLocalNightMode(mode)
     }
 
     private fun initViews(savedInstanceState: Bundle?) {
@@ -69,7 +76,9 @@ class ProfileActivity : AppCompatActivity(){
         showCurrentMode(isEditMode)
 
         btn_edit.setOnClickListener {
-            if (isEditMode) saveProfileInfo()
+            if (isEditMode) {
+                saveProfileInfo()
+            }
             isEditMode = !isEditMode
             showCurrentMode(isEditMode)
         }
@@ -77,23 +86,24 @@ class ProfileActivity : AppCompatActivity(){
         btn_switch_theme.setOnClickListener {
             viewModel.switchTheme()
         }
-
     }
 
     private fun showCurrentMode(isEdit: Boolean) {
-        val info = viewFields.filter { setOf("firstName", "lastName", "about", "repository").contains(it.key) }
+        val info = viewFields.filter {
+            setOf("firstName", "lastName", "about", "repository").contains(it.key)
+        }
         for ((_, v) in info) {
             v as EditText
             v.isFocusable = isEdit
             v.isFocusableInTouchMode = isEdit
             v.isEnabled = isEdit
-            v.background.alpha = if(isEdit) 255 else 0
+            v.background.alpha = if (isEdit) 255 else 0
         }
 
         ic_eye.visibility = if (isEdit) View.GONE else View.VISIBLE
         wr_about.isCounterEnabled = isEdit
 
-        with(btn_edit) {
+        with (btn_edit) {
             val filter: ColorFilter? = if (isEdit) {
                 PorterDuffColorFilter(
                     resources.getColor(R.color.color_accent, theme),
@@ -112,12 +122,6 @@ class ProfileActivity : AppCompatActivity(){
             background.colorFilter = filter
             setImageDrawable(icon)
         }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putBoolean(IS_EDIT_MODE, isEditMode)
-
     }
 
     private fun saveProfileInfo() {
